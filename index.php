22f6b72d611d5ae771cea7156adb0a0d3ba73659
Rik Bakker <ra.bakkeralfacollege@gmail.com>
2023-08-19 19:10:22 +0200

    began with coding

--- index.php
+++ index.php
@@ -1,1 +1,48 @@
-fjsdkl
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rik Arjen Bakker</title>
+
+    <link rel="stylesheet" href="css/style.css" >
+  </head>
+  <body>
+    <div class="home">
+      <div class="home-name">
+        Rik Arjen Bakker
+      </div>
+      <div class="home-img">
+        <img src="img/RikBakker.JPG" alt="Rik Arjen Bakker">
+      </div>
+      <div class="home-info">
+        <div class="home-info-subtext">
+          <div class="home-info-subtext-text">
+            Website Building
+          </div>
+          <div class="home-info-subtext-text">
+            Student
+          </div>
+          <div class="home-info-subtext-text">
+            Human Being
+          </div>
+        </div>
+        <div class="home-info-maintext">
+          Software Developer Student
+        </div>
+      </div>
+      <div class="home-menu">
+        <div class="home-menu-item">
+          About Me
+        </div>
+        <div class="home-menu-item">
+          My Work
+        </div>
+        <div class="home-menu-item">
+          Contact
+        </div>
+      </div>
+    </div>
+  </body> 
+</html>
