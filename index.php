4b7445ed946f2da999f1e2c18384a34b1405681d
Rik Bakker <ra.bakkeralfacollege@gmail.com>
2023-09-05 10:03:19 +0200

    saving current status

--- index.php
+++ index.php
@@ -9,40 +9,90 @@
     <link rel="stylesheet" href="css/style.css" >
   </head>
   <body>
-    <div class="home">
-      <div class="home-name">
-        Rik Arjen Bakker
-      </div>
-      <div class="home-img">
-        <img src="img/RikBakker.JPG" alt="Rik Arjen Bakker">
-      </div>
-      <div class="home-info">
-        <div class="home-info-subtext">
-          <div class="home-info-subtext-text">
-            Website Building
-          </div>
-          <div class="home-info-subtext-text">
-            Student
-          </div>
-          <div class="home-info-subtext-text">
-            Human Being
+
+    <div class="home-div">
+      <div class="home">
+        <img class="background-img-home" src="img\stone-brick-background.jpg">
+        <div class="home-name-div">
+          <div class="home-name">
+            Rik Arjen Bakker
           </div>
         </div>
-        <div class="home-info-maintext">
-          Software Developer Student
+        <div class="home-img">
+          <img src="img/RikBakker.JPG" alt="Rik Arjen Bakker">
+        </div>
+        <div class="home-info">
+          <div class="home-info-subtext">
+            <div class="home-info-subtext-text">
+              Website Building
+            </div>
+            <div class="home-info-subtext-text">
+              Student
+            </div>
+            <div class="home-info-subtext-text">
+              Human Being
+            </div>
+          </div>
+          <div class="home-info-maintext">
+            Software Developer Student
+          </div>
         </div>
       </div>
       <div class="home-menu">
-        <div class="home-menu-item">
-          About Me
-        </div>
-        <div class="home-menu-item">
-          My Work
-        </div>
-        <div class="home-menu-item">
-          Contact
+        <div class="home-menu-div">
+          <a href="">
+            <div class="home-menu-item">
+            About Me
+            </div>
+          </a>
+          <div class="home-menu-spacer">
+            |
+          </div>
+          <a href="">
+            <div class="home-menu-item">
+              My Work
+            </div>
+          </a>
+          <div class="home-menu-spacer">
+            |
+          </div>
+          <a href="">
+            <div class="home-menu-item">
+              Contact
+            </div>
+          </a>
         </div>
       </div>
     </div>
+
+    <div class="AboutMe">
+      <img class="background-img-AboutMe" src="img\paper-3338041.png">
+      <div class="AboutMe-title-div">
+        <div class="AboutMe-title">
+          About Me
+        </div>
+      </div>
+      <div class="AboutMe-bio">
+        I am a 2nd year software development student at Alfa-College based in Groningen, the  Netherlands. I am interested in designing and coding websites and applications and love helping people out.
+      </div>
+      <div class="AboutMe-loves">
+       I love
+        - Helping people
+        - Being challenged
+        - Good conversation 
+        - Technology
+        - Coffee
+      </div>
+      <div class="AboutMe-haves">
+        I Have
+        - A transition certificate havo 4-5
+        - Won the 5G Student Battle 2022
+      </div>
+    </div>
+
+    <div class="contact">
+      
+    </div>
+
   </body> 
 </html>
