5bcb9b8403695153253d2e76eb0bfeac489d96b5
Rik Bakker <ra.bakkeralfacollege@gmail.com>
2023-09-05 14:15:22 +0200

    made the basic html and css

--- index.php
+++ index.php
@@ -69,29 +69,78 @@
       <img class="background-img-AboutMe" src="img\paper-3338041.png">
       <div class="AboutMe-title-div">
         <div class="AboutMe-title">
-          About Me
+          <p>
+            About Me
+          </p>
         </div>
       </div>
-      <div class="AboutMe-bio">
-        I am a 2nd year software development student at Alfa-College based in Groningen, the  Netherlands. I am interested in designing and coding websites and applications and love helping people out.
-      </div>
-      <div class="AboutMe-loves">
-       I love
-        - Helping people
-        - Being challenged
-        - Good conversation 
-        - Technology
-        - Coffee
-      </div>
-      <div class="AboutMe-haves">
-        I Have
-        - A transition certificate havo 4-5
-        - Won the 5G Student Battle 2022
+      <div class="AboutMe-div">
+        <div class="AboutMe-bio">
+          I am a 2nd year software development student at Alfa-College based in Groningen, the  Netherlands. I am interested in designing and coding websites and applications and love helping people out.
+        </div>
+        <div class="AboutMe-loves">
+         I love <br>
+          - Helping people <br>
+          - Being challenged <br>
+          - Good conversation <br>
+          - Technology <br>
+          - Coffee <br>
+        </div>
+        <div class="AboutMe-haves">
+          I Have <br>
+          - A transition certificate havo 4-5 <br>
+          - Won the 5G Student Battle 2022
+        </div>
       </div>
     </div>
 
-    <div class="contact">
-      
+    <div class="MyWork">
+      <img class="background-img-MyWork" src="img\stone-brick-background.jpg">
+      <div class="MyWork-title-div">
+        <div class="MyWork-title">
+          <p>
+            My Work
+          </p>
+        </div>
+      </div>
+      <div class="MyWork-div">
+        <div class="MyWork-text">
+          <p>
+            I have a good basic knowledge of  HTML, CSS, JavaScript and vanilla PHP
+          </p>
+        </div>  
+        <div class="MyWork-links">
+          <a href="">- Team LoFlow Website</a><br>
+          <a href="">- Website (Schoolproject)</a><br>
+          <a href="">- My Portfolio Website</a><br>
+          <a href="">- My GitHub</a><br>
+        </div>
+      </div>
+    </div>
+
+    <div class="Contact">
+      <img class="background-img-Contact" src="img\stone-brick-background.jpg">
+      <div class="Contact-title-div">
+        <div class="Contact-title">
+          <p>
+            Contact
+          </p>
+        </div>
+      </div>
+      <div class="Contact-text">
+        You can contact me trough the following channels
+      </div>
+      <div class="Contact-links">
+        <a href="https://www.linkedin.com/in/rik-bakker-36904a290/">
+          <img src="img\LI-In-Bug.png" alt="linkedin">
+        </a>
+        <a href="https://github.com/rikarjenbakker">
+          <img src="img\github-mark-white.png" alt="">
+        </a>
+        <a href=mailto:"ra.bakkeralfacollege@gmail.com">
+          <img src="img\Gmail_Logo_512px.png" alt="">
+        </a>
+      </div>
     </div>
 
   </body> 
